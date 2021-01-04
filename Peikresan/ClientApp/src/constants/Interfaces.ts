86aa3c477834ecee0e13504fabb316b1779a8c63
mohsen <mohsen1299@gmail.com>
2021-01-04 12:23:48 +0330

    start making global interfaces

---
+++ Peikresan/ClientApp/src/constants/Interfaces.ts
@@ -0,0 +1,11 @@
+export interface IMoreBtn{
+    show:boolean;
+    title:string;
+    link:string;
+  }
+
+  export interface ICategory{
+      id: number;
+      title: string;
+      img:string;
+  }
