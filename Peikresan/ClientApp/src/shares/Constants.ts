8f3275f712978c1af141f7665f0af9a0f44ac688
mohsen <mohsen1299@gmail.com>
2021-01-12 22:18:39 +0330

    add some routes enums

--- Peikresan/ClientApp/src/shares/Constants.ts
+++ Peikresan/ClientApp/src/shares/Constants.ts
@@ -8,10 +8,41 @@ export enum UserRoles {
 }
 
 export enum HomePath {
+  Home = "/",
+  Categories = "/categories",
+  Addresses = "/addresses",
+  Factors = "/factors",
+  FAQ = "/faq",
+  About = "/about",
+  Coin = "/coin",
+  Profile = "/profile",
+
   Category = "/category/",
   Suggestions = "#",
   MostSells = "#",
   Newest = "#",
+}
+
+export enum AdminPath {
+  Admin = "/admin",
+  Login = "/admin",
+  Dashboard = "/admin/dashboard",
+  Categories = "/admin/categories",
+  Products = "/admin/products",
+  AwesomeProducts = "/admin/awesome_products",
+  Sliders = "/admin/sliders",
+  Banners = "/admin/banners",
+  Users = "/admin/users",
+  ActiveOrders = "/admin/active-orders",
+  SellerProducts = "/admin/seller-products",
+  SellerOrders = "/admin/seller-orders",
+  DeliverOrders = "/admin/deliver-orders",
+  Factors = "/admin/factors",
+  // Logout = "/admin/logout",
+}
+
+export enum CartPath {
+  Cart = "/cart",
 }
 
 export const DefaultAddress: IAddress = {
