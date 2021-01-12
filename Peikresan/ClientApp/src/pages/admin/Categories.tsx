6a7a50961c8c2189e39a35f267eb66293ff2750d
mohsen <mohsen1299@gmail.com>
2021-01-12 23:22:21 +0330

    add admin category pages

---
+++ Peikresan/ClientApp/src/pages/admin/Categories.tsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Table, Tag, Space, Popconfirm, message, Tooltip, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
+import MyPrivateLayout from "../../components/MyPrivateLayout";
+import { ICategory } from "../../shares/Interfaces";
+import { ApplicationState } from "../../store";
+import { actionCreators } from "../../store/Auth";
+import { AdminPath, Status } from "../../shares/Constants";
+import { AdminDataModel, AdminDataUrl } from "../../shares/URLs";
+
+interface ICategoriesProps {
+  categories: ICategory[];
+  status: Status;
+
+  RemoveElement: Function;
+  ResetStatus: Function;
+}
+
+const Categories: React.FC<ICategoriesProps> = ({
+  categories,
+  status,
+  RemoveElement,
+  ResetStatus,
+}) => {
+  React.useEffect(() => {
+    if (status == Status.SUCCEEDED) {
+      message.success("با موفقیت حذف شد.");
+      ResetStatus();
+    } else if (status == Status.FAILED) {
+      message.error("اشکال در حذف");
+      ResetStatus();
+    }
+  }, [status]);
+
+  const columns = [
+    {
+      title: "شماره",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "عنوان دسته",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "تصویر دسته",
+      dataIndex: "img",
+      key: "img",
+      render: (img: string, record: ICategory) => (
+        <Space size="middle">
+          <img style={{ maxWidth: "75px" }} src={img} alt={record.title} />
+        </Space>
+      ),
+    },
+    {
+      title: "عملیات",
+      key: "action",
+      render: (_: any, record: ICategory) => (
+        <Space size="middle">
+          <Popconfirm
+            title="از حذف اطمینان دارید؟"
+            onConfirm={(e) => {
+              if (status == Status.LOADING) return;
+              RemoveElement(
+                AdminDataUrl.REMOVE_BANNER_URL,
+                AdminDataModel.Banners,
+                record.id
+              );
+            }}
+            onCancel={(e) => {
+              console.log("cancel delete");
+            }}
+            okText="بله"
+            cancelText="خیر"
+          >
+            <Tag color="red">حذف</Tag>
+          </Popconfirm>
+          <Link to={AdminPath.Category + record.id}>
+            <Tag
+              color="blue"
+              // onClick={() => {
+              //   console.log("edit cat", record.id);
+              // }}
+            >
+              تغییر
+            </Tag>
+          </Link>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <MyPrivateLayout>
+      <div>
+        <h1>لیست دسته‌بندی‌ها</h1>
+
+        <Tooltip title="دسته‌بندی جدید">
+          <Link to={AdminPath.Category} className="float-add-btn">
+            <Button type="primary" shape="circle" icon={<PlusOutlined />} />
+          </Link>
+        </Tooltip>
+
+        <Table columns={columns} dataSource={categories} pagination={false} />
+      </div>
+    </MyPrivateLayout>
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({
+  categories: state.auth ? state.auth.categories : [],
+  status: state.auth ? state.auth.status : Status.INIT,
+});
+
+const mapDispatchToProps = {
+  RemoveElement: actionCreators.removeElement,
+  ResetStatus: actionCreators.resetStatus,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Categories);
---
+++ Peikresan/ClientApp/src/pages/admin/Category.tsx
@@ -0,0 +1,173 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
+import { Input, Space, Button, AutoComplete, message } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+
+import MyPrivateLayout from "../../components/MyPrivateLayout";
+import { ApplicationState } from "../../store";
+import { ICategory } from "../../shares/Interfaces";
+import { actionCreators } from "../../store/Auth";
+import { AdminDataModel, AdminDataUrl, LOGIN_URL } from "../../shares/URLs";
+import { AdminPath, Status } from "../../shares/Constants";
+
+import "./Admin.css";
+
+const { TextArea } = Input;
+
+interface ICategoryProps {
+  categories: ICategory[];
+  status: Status;
+
+  AddOrChangeElement: Function;
+  ResetStatus: Function;
+}
+
+interface IParamTypes {
+  id: string;
+}
+
+const Category: React.FC<ICategoryProps> = ({
+  categories,
+  status,
+  AddOrChangeElement,
+  ResetStatus,
+}) => {
+  const { id } = useParams<IParamTypes>();
+
+  React.useEffect(() => {
+    if (status == Status.SUCCEEDED) {
+      const history = useHistory();
+      history.push(AdminPath.Categories);
+      message.success("با موفقیت ذخیره شد.");
+    } else if (status == Status.FAILED) {
+      message.error("اشکال در ذخیره");
+    }
+    return ResetStatus();
+  }, [status]);
+
+  const [file, setFile] = React.useState<File>();
+  const [title, setTitle] = React.useState<string>();
+  const [category, setCategory] = React.useState<string>();
+  const [description, setDescription] = React.useState<string>();
+  const [showImage, setShowImage] = React.useState<string>();
+
+  const validateInputs = () => title && title.length > 1;
+
+  if (id !== undefined) {
+    const category = categories.find((c) => c.id === Number(id));
+    // console.log(id, category, title);
+    if (category !== undefined && title === undefined) {
+      setTitle(category.title);
+      setDescription(category.description);
+      setShowImage(category.img);
+    }
+  }
+
+  const sendData = () => {
+    if (!validateInputs() || status == Status.LOADING) return;
+
+    var formData = new FormData();
+    formData.append("id", id);
+    formData.append("file", file);
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("category", category);
+
+    AddOrChangeElement(
+      AdminDataUrl.ADD_CHANGE_CATEGORY_URL,
+      AdminDataModel.Categories,
+      formData
+    );
+  };
+
+  return (
+    <MyPrivateLayout>
+      <div className="admin-container">
+        <h1>دسته بندی</h1>
+        <Space direction="vertical">
+          <img style={{ maxWidth: "300px" }} src={showImage} alt="تصویر دسته" />
+          <input
+            id="product-img"
+            type="file"
+            style={{ display: "none" }}
+            onChange={(e) => {
+              const files = e.target.files;
+              if (files) {
+                const file = files[0];
+                setFile(file);
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                  const result = e.target?.result;
+                  if (result) {
+                    setShowImage(result as string);
+                  }
+                };
+                reader.readAsDataURL(file);
+              }
+            }}
+          />
+          <Button
+            style={{ minWidth: "350px" }}
+            icon={<UploadOutlined />}
+            onClick={() => {
+              document.getElementById("product-img").click();
+            }}
+          >
+            بارگزاری عکس
+          </Button>
+
+          <Input
+            placeholder="نام دسته‌بندی جدید"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <TextArea
+            rows={4}
+            value={description}
+            placeholder="توضیحات"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+
+          <AutoComplete
+            style={{ minWidth: "350px" }}
+            options={categories.map((cat) => ({
+              value: cat.title,
+            }))}
+            placeholder=" نام دسته بندی والد"
+            filterOption={(inputValue, option) =>
+              option.value.indexOf(inputValue) !== -1
+            }
+            onChange={(value) => {
+              setCategory(value);
+            }}
+          />
+
+          <Button
+            type="primary"
+            disabled={!validateInputs()}
+            onClick={sendData}
+          >
+            ذخیره
+          </Button>
+        </Space>
+      </div>
+    </MyPrivateLayout>
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({
+  categories: state.auth?.categories ?? [],
+  status: state.auth?.status ?? Status.INIT,
+});
+
+const mapDispatchToProps = {
+  AddOrChangeElement: actionCreators.addOrChangeElement,
+  ResetStatus: actionCreators.resetStatus,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Category);
