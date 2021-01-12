ef8534764b9753089cd737d876091a849b485f0c
mohsen <mohsen1299@gmail.com>
2021-01-12 12:58:16 +0330

    make banners page

---
+++ Peikresan/ClientApp/src/pages/admin/Banners.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Table, Tag, Space, Popconfirm, message, Tooltip, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
+import MyPrivateLayout from "../../components/MyPrivateLayout";
+import { IBanner } from "../../shares/Interfaces";
+import { ApplicationState } from "../../store";
+import { actionCreators } from "../../store/Auth";
+import { Status } from "../../shares/Constants";
+import { AdminDataModel, AdminDataUrl, LOGIN_URL } from "../../shares/URLs";
+
+import "./Admin.css";
+
+interface IBannerProps {
+  banners: IBanner[];
+  status: Status;
+
+  RemoveElement: Function;
+}
+
+const Banners: React.FC<IBannerProps> = ({
+  banners,
+  status,
+  RemoveElement,
+}) => {
+  const columns = [
+    {
+      title: "شماره",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "عنوان",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "تصویر",
+      dataIndex: "img",
+      key: "img",
+      render: (img, record) => (
+        <Space size="middle">
+          <img style={{ maxWidth: "75px" }} src={img} alt={record.title} />
+        </Space>
+      ),
+    },
+    {
+      title: "عملیات",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Popconfirm
+            title="از حذف اطمینان دارید؟"
+            onConfirm={() => {
+              if (status == Status.LOADING) return;
+              RemoveElement(
+                AdminDataUrl.REMOVE_BANNER_URL,
+                AdminDataModel.Banners,
+                record.id
+              );
+            }}
+            onCancel={(e) => {
+              console.log("cancel delete");
+            }}
+            okText="بله"
+            cancelText="خیر"
+          >
+            <Tag color="red">حذف</Tag>
+          </Popconfirm>
+          <Link to={"/admin/banner/" + record.id}>
+            <Tag color="blue">تغییر</Tag>
+          </Link>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <MyPrivateLayout>
+      <div>
+        <h1>لیست بنرها</h1>
+
+        <Tooltip title="بنر جدید">
+          <Link to="/admin/banner" className="float-add-btn">
+            <Button type="primary" shape="circle" icon={<PlusOutlined />} />
+          </Link>
+        </Tooltip>
+
+        <Table columns={columns} dataSource={banners} pagination={false} />
+      </div>
+    </MyPrivateLayout>
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({
+  banners: state.auth ? state.auth.banners : [],
+  status: state.auth ? state.auth.status : Status.INIT,
+});
+
+const mapDispatchToProps = {
+  RemoveElement: actionCreators.removeElement,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Banners);
