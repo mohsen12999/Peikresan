3b8caebb326e267145746e9a2de561baa81ee944
mohsen <mohsen1299@gmail.com>
2021-01-13 23:45:42 +0330

    add product and seller product

---
+++ Peikresan/ClientApp/src/pages/admin/SellerProduct.tsx
@@ -0,0 +1,130 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useParams, useHistory, Link } from "react-router-dom";
+import { Space, Button, InputNumber, message, AutoComplete } from "antd";
+
+import MyPrivateLayout from "../../components/MyPrivateLayout";
+import { ApplicationState } from "../../store";
+import { IProduct, ISellerProduct } from "../../shares/Interfaces";
+import { actionCreators } from "../../store/Auth";
+import { AdminPath, AdminDataUrl, LOGIN_URL } from "../../shares/URLs";
+import { AdminDataModel, Status } from "../../shares/Constants";
+
+import "./Admin.css";
+
+interface ISellerProductProps {
+  sellerProducts: ISellerProduct[];
+  products: IProduct[];
+  status: Status;
+
+  AddOrChangeElement: Function;
+  ResetStatus: Function;
+}
+
+interface IParamTypes {
+  id: string;
+}
+
+const SellerProduct: React.FC<ISellerProductProps> = ({
+  sellerProducts,
+  products,
+  status,
+  AddOrChangeElement,
+  ResetStatus,
+}) => {
+  const { id } = useParams<IParamTypes>();
+
+  React.useEffect(() => {
+    if (status == Status.SUCCEEDED) {
+      const history = useHistory();
+      history.push(AdminPath.SellerProducts);
+      message.success("با موفقیت ذخیره شد.");
+    } else if (status == Status.FAILED) {
+      message.error("اشکال در ذخیره");
+    }
+    return ResetStatus();
+  }, [status]);
+
+  const [product, setProduct] = React.useState<string>();
+  const [count, setCount] = React.useState<number>();
+
+  const validateInputs = () => product && product.length > 1 && count;
+
+  if (id !== undefined) {
+    const sellerProduct = sellerProducts.find((p) => p.id === Number(id));
+    if (sellerProduct !== undefined && product === undefined) {
+      setProduct(sellerProduct.productTitle);
+      setCount(sellerProduct.count);
+    }
+  }
+
+  const sendData = () => {
+    if (!validateInputs() || status == Status.LOADING) return;
+
+    var formData = new FormData();
+    formData.append("id", id);
+    formData.append("product", product ?? "");
+    formData.append("count", String(count));
+
+    AddOrChangeElement(
+      AdminDataUrl.ADD_CHANGE_SELLER_PRODUCT_URL,
+      AdminDataModel.SellerProducts,
+      formData
+    );
+  };
+
+  return (
+    <MyPrivateLayout>
+      <div className="admin-container">
+        <h1>محصول</h1>
+        <Space direction="vertical">
+          <AutoComplete
+            style={{ minWidth: "350px" }}
+            options={products.map((prod) => ({
+              value: prod.title,
+            }))}
+            placeholder=" نام محصول"
+            filterOption={(inputValue, option) =>
+              option.value.indexOf(inputValue) !== -1
+            }
+            onChange={(value) => {
+              setProduct(value);
+            }}
+          />
+
+          <InputNumber
+            className="input-style"
+            value={count}
+            placeholder="تعداد"
+            onChange={(value) => {
+              setCount(Number(value));
+            }}
+          />
+
+          <br />
+
+          <Button
+            type="primary"
+            disabled={!validateInputs()}
+            onClick={sendData}
+          >
+            ذخیره
+          </Button>
+        </Space>
+      </div>
+    </MyPrivateLayout>
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({
+  sellerProducts: state.auth?.sellerProducts ?? [],
+  products: state.auth?.products ?? [],
+  status: state.auth?.status ?? Status.INIT,
+});
+
+const mapDispatchToProps = {
+  AddOrChangeElement: actionCreators.addOrChangeElement,
+  ResetStatus: actionCreators.resetStatus,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SellerProduct);
---
+++ Peikresan/ClientApp/src/pages/admin/SellerProducts.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Table, Tag, Space, Popconfirm, message, Tooltip, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
+import MyPrivateLayout from "../../components/MyPrivateLayout";
+import { ISellerProduct } from "../../shares/Interfaces";
+import { ApplicationState } from "../../store";
+import { actionCreators } from "../../store/Auth";
+import { AdminDataModel, Status } from "../../shares/Constants";
+import { AdminPath, AdminDataUrl } from "../../shares/URLs";
+
+import "./Admin.css";
+
+interface ISellerProductsProps {
+  sellerProducts: ISellerProduct[];
+  status: Status;
+
+  RemoveElement: Function;
+  ResetStatus: Function;
+}
+
+const SellerProducts: React.FC<ISellerProductsProps> = ({
+  sellerProducts,
+  status,
+  RemoveElement,
+  ResetStatus,
+}) => {
+  React.useEffect(() => {
+    if (status == Status.SUCCEEDED) {
+      message.success("با موفقیت حذف شد.");
+      ResetStatus();
+    } else if (status == Status.FAILED) {
+      message.error("اشکال در حذف");
+      ResetStatus();
+    }
+  }, [status]);
+
+  const columns = [
+    {
+      title: "نام کالا",
+      key: "product",
+      render: (text: any, record: ISellerProduct) => (
+        <Space size="middle">
+          {record.productId + " - " + record.productTitle}
+        </Space>
+      ),
+    },
+    {
+      title: "تعداد",
+      dataIndex: "count",
+      key: "count",
+    },
+    {
+      title: "عملیات",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Popconfirm
+            title="از حذف اطمینان دارید؟"
+            onConfirm={(e) => {
+              if (status == Status.LOADING) return;
+              RemoveElement(
+                AdminDataUrl.REMOVE_SELLER_PRODUCT_URL,
+                AdminDataModel.SellerProducts,
+                record.id
+              );
+            }}
+            onCancel={(e) => {
+              console.log("cancel delete");
+            }}
+            okText="بله"
+            cancelText="خیر"
+          >
+            <Tag color="red">حذف</Tag>
+          </Popconfirm>
+          <Link to={AdminPath.SellerProduct + record.id}>
+            <Tag color="blue">تغییر</Tag>
+          </Link>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <MyPrivateLayout>
+      <div>
+        <h1>لیست کالاها</h1>
+
+        <Tooltip title="کالای جدید">
+          <Link to={AdminPath.SellerProduct} className="float-add-btn">
+            <Button type="primary" shape="circle" icon={<PlusOutlined />} />
+          </Link>
+        </Tooltip>
+
+        {sellerProducts.length > 0 ? (
+          <Table
+            columns={columns}
+            dataSource={sellerProducts}
+            pagination={false}
+          />
+        ) : (
+          <div>
+            <h3>برای شما کالایی ثبت نشده</h3>
+            <Link to={AdminPath.SellerProduct}>
+              <Button type="primary">ثبت کالا برای فروشگاه</Button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </MyPrivateLayout>
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({
+  sellerProducts: state.auth ? state.auth.sellerProducts : [],
+  status: state.auth ? state.auth.status : Status.INIT,
+});
+
+const mapDispatchToProps = {
+  RemoveElement: actionCreators.removeElement,
+  ResetStatus: actionCreators.resetStatus,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SellerProducts);
