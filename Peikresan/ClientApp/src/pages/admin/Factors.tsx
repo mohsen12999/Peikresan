904826a4cb2084b0bde24bb84cfb493208e40f48
mohsen <mohsen1299@gmail.com>
2021-01-13 20:08:18 +0330

    add factor page

---
+++ Peikresan/ClientApp/src/pages/admin/Factors.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Table, Space } from "antd";
+
+import MyPrivateLayout from "../../components/MyPrivateLayout";
+import { OrderStatusDescription } from "../../shares/services/OrderFunctions";
+import { ApplicationState } from "../../store";
+import { IFactor } from "../../shares/Interfaces";
+
+import "./Admin.css";
+
+interface IFactorsProps {
+  factors: IFactor[];
+}
+
+const Factors: React.FC<IFactorsProps> = ({ factors }) => {
+  const columns = [
+    {
+      title: "شماره",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "نام خریدار",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "وضعیت سفارش",
+      key: "orderStatus",
+      render: (_: any, record: IFactor) => (
+        <Space size="middle">
+          {OrderStatusDescription(record.factorStatusDescription)}
+        </Space>
+      ),
+    },
+    {
+      title: "مجموع قیمت",
+      dataIndex: "totalPrice",
+      key: "totalPrice",
+    },
+    {
+      title: "شماره پیگیری",
+      dataIndex: "traceNumber",
+      key: "traceNumber",
+    },
+  ];
+
+  return (
+    <MyPrivateLayout>
+      <div>
+        <h1>لیست فاکتور‌ها</h1>
+        {factors.length > 0 ? (
+          <Table columns={columns} dataSource={factors} pagination={false} />
+        ) : (
+          <h2>فاکتوری برای شما وجود ندارد</h2>
+        )}
+      </div>
+    </MyPrivateLayout>
+  );
+};
+
+const mapStateToProps = (state: ApplicationState) => ({
+  factors: state.auth ? state.auth.factors : [],
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Factors);
