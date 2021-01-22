5043e0a648e39f87df4dd37a388569d4a509e129
mohsen <mohsen1299@gmail.com>
2021-01-22 22:43:55 +0330

    declare module to prevent typescript error from neshan map

---
+++ Peikresan/ClientApp/src/typing.d.ts
@@ -0,0 +1,10 @@
+// declare module "@ckeditor/ckeditor5-react" {
+//     export const CKEditor: Function;
+//   }
+//   declare module "@ckeditor/ckeditor5-build-decoupled-document" {}
+
+declare module "react-neshan-map-leaflet" {
+  import * as React from "react";
+  const NeshanMap: React.FC<any>;
+  export default NeshanMap;
+}
