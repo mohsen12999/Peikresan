dbd4366ba73c8664c30d09bd61544ff20216082d
mohsen <mohsen1299@gmail.com>
2021-02-08 17:04:11 +0330

    make client model for output data

---
+++ Peikresan/Data/ClientModels/ClientProduct.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peikresan.Data.ClientModels
+{
+    public class ClientProduct
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+
+        public decimal Price { get; set; }
+        public int Max { get; set; }
+        public bool SoldByWeight { get; set; }
+        public int MinWeight { get; set; }
+
+        public int Barcode { get; set; }
+        public int Order { get; set; }
+
+        public int? CategoryId { get; set; }
+        public string Category { get; set; }
+    }
+}
+
+/*
+  id: number;
+  title: string;
+  description: string;
+  img: string;
+  price: number;
+  max: number;
+  soldByWeight: boolean;
+  minWeight: number;
+
+  barcode: number;
+  order: number;
+
+  categoryId: number;
+  category: string;
+*/
---
+++ Peikresan/Data/ClientModels/ClientUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peikresan.Data.ClientModels
+{
+    public class ClientUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string Role { get; set; }
+        public string RoleId { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
+/*
+export interface IUser {
+  id: string;
+  fullName: string;
+
+  userName: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  mobile: string;
+  Address: string;
+
+  role: string;
+  roleId: string;
+}
+ */
