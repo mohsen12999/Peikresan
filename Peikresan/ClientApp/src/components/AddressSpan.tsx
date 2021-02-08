d8abb9ee6406716d6da22fb99b1a58794a9bd944
mohsen <mohsen1299@gmail.com>
2021-02-08 17:10:12 +0330

    change address span base on new interface

--- Peikresan/ClientApp/src/components/AddressSpan.tsx
+++ Peikresan/ClientApp/src/components/AddressSpan.tsx
@@ -4,26 +4,26 @@ import { IAddress } from "../shares/Interfaces";
 const AddressSpan: React.FC<IAddress> = ({
   state,
   city,
-  district,
+  // district,
   mobile,
   name,
   description,
-  level,
-  unit,
-  number,
-  postalCode,
+  // level,
+  // unit,
+  // number,
+  // postalCode,
 }) => (
   <React.Fragment>
     <div>
-      {state + ", " + city + ", " + district + ", موبایل: " + mobile}
+      {state + ", " + city + ", موبایل: " + mobile}
       {name && name.length > 0 && ", گیرنده: " + name}
     </div>
     <div>
       {description}
-      {level && level.length > 0 && ", طبقه: " + level}
+      {/* {level && level.length > 0 && ", طبقه: " + level}
       {unit && unit.length > 0 && ", واحد: " + unit}
       {number && number.length > 0 && ", پلاک: " + number}
-      {postalCode && postalCode.length > 0 && ", کدپستی: " + postalCode}
+      {postalCode && postalCode.length > 0 && ", کدپستی: " + postalCode} */}
     </div>
   </React.Fragment>
 );
