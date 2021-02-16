67f2d7b0c276b773a0e6f939a3e89bc32592bad6
mohsen <mohsen1299@gmail.com>
2021-02-17 01:27:03 +0330

    some price calculation

---
+++ Peikresan/Services/PriceServices.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Peikresan.Data.Models;
+
+namespace Peikresan.Services
+{
+    public static class PriceServices
+    {
+        // TODO: Read from Setting
+        public static readonly decimal MinimumCart  = 70000;
+        public static readonly decimal DeliverPrice  = 5000;
+        public static readonly decimal DeliverAtDoor = 2000;
+        public static readonly decimal ExpressDeliver = 10000;
+
+        public static decimal CalculatePrice(List<OrderItem> orderItems, bool deliverAtDoor , bool expressDeliver = false)
+        {
+            var totalPrice = orderItems.Sum(item => item.ItemsPrice);
+            if (expressDeliver)
+                return totalPrice + ExpressDeliver;
+
+            if (totalPrice < MinimumCart) totalPrice += DeliverPrice;
+            if (deliverAtDoor) totalPrice += DeliverAtDoor;
+
+            return totalPrice;
+        }
+    }
+}
