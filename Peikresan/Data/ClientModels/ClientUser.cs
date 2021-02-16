6f8eeaefc209cdf462fe41830353385f85bcbf3c
mohsen <mohsen1299@gmail.com>
2021-02-17 01:35:14 +0330

    add title for client user model

--- Peikresan/Data/ClientModels/ClientUser.cs
+++ Peikresan/Data/ClientModels/ClientUser.cs
@@ -8,6 +8,7 @@ namespace Peikresan.Data.ClientModels
     public class ClientUser
     {
         public string Id { get; set; }
+        public string Title { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
