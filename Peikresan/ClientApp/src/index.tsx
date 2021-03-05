3f4850710b7dbdf14c6fccbb301ce97b3c53bb16
mohsen <mohsen1299@gmail.com>
2021-03-05 20:35:13 +0330

    update service worker

--- Peikresan/ClientApp/src/index.tsx
+++ Peikresan/ClientApp/src/index.tsx
@@ -5,7 +5,7 @@ import { ConnectedRouter } from "connected-react-router";
 import { createBrowserHistory } from "history";
 import configureStore from "./store/configureStore";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 // Create browser history to use in the Redux store
 const baseUrl = document
@@ -25,4 +25,21 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register({
+  onUpdate: async (registration: any) => {
+    // We want to run this code only if we detect a new service worker is
+    // waiting to be activated.
+    // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      // Makes Workbox call skipWaiting()
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      // Once the service worker is unregistered, we can reload the page to let
+      // the browser download a fresh copy of our app (invalidating the cache)
+      window.location.reload();
+    }
+  },
+});
