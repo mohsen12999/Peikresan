bf48a36b6c4847d2bf414ae22abaa0ce2121b5d0
mohsen <mohsen1299@gmail.com>
2021-03-05 18:37:44 +0330

    encode and decode id number in url

---
+++ Peikresan/Services/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Peikresan.Services
+{
+    public static class Helper
+    {
+        public static string EncodeNumber(int number)
+        {
+            var bytes = BitConverter.GetBytes(number);
+            var base64String = Convert.ToBase64String(bytes);
+            var encodedString = System.Web.HttpUtility.UrlEncode(base64String);
+
+            return encodedString;
+        }
+
+        public static int DecodeNumber(string code)
+        {
+            var decodedString = System.Web.HttpUtility.UrlDecode(code);
+            var bytes = Convert.FromBase64String(decodedString);
+            var number = BitConverter.ToInt32(bytes, 0);
+
+            return number;
+        }
+    }
+}
