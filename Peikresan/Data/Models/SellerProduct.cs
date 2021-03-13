8c58cb488145efbdf93c4c4c6fb246d23c070eac
mohsen <mohsen1299@gmail.com>
2021-03-13 09:33:01 +0330

    add barcode and title for seller product model

--- Peikresan/Data/Models/SellerProduct.cs
+++ Peikresan/Data/Models/SellerProduct.cs
@@ -5,15 +5,20 @@ namespace Peikresan.Data.Models
 {
     public class SellerProduct
     {
-        public Guid? UserId { get; set; }
-        public User User { get; set; }
-
-        public int? ProductId { get; set; }
-        public Product Product { get; set; }
+        public string Title { get; set; }
 
         public int Count { get; set; }
 
         [Column(TypeName = "decimal(10,3)")]
         public decimal Price { get; set; }
+
+        [Column(TypeName = "decimal(10,0)")]
+        public decimal? Barcode { get; set; }
+
+        public Guid? UserId { get; set; }
+        public User User { get; set; }
+
+        public int? ProductId { get; set; }
+        public Product Product { get; set; }
     }
 }
