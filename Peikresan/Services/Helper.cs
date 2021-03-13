bb4dc8181e24d9207663019e5903c7ebd0ccc228
mohsen <mohsen1299@gmail.com>
2021-03-13 09:33:28 +0330

    change url encoded function in helper

--- Peikresan/Services/Helper.cs
+++ Peikresan/Services/Helper.cs
@@ -4,22 +4,29 @@ namespace Peikresan.Services
 {
     public static class Helper
     {
-        public static string EncodeNumber(int number)
-        {
-            var bytes = BitConverter.GetBytes(number);
-            var base64String = Convert.ToBase64String(bytes);
-            var encodedString = System.Web.HttpUtility.UrlEncode(base64String);
 
-            return encodedString;
-        }
+        public static string EncodeNumber(int number) => 
+            Microsoft.AspNetCore.WebUtilities.Base64UrlTextEncoder.Encode(BitConverter.GetBytes(number));
 
-        public static int DecodeNumber(string code)
-        {
-            var decodedString = System.Web.HttpUtility.UrlDecode(code);
-            var bytes = Convert.FromBase64String(decodedString);
-            var number = BitConverter.ToInt32(bytes, 0);
+        public static int DecodeNumber(string code) =>
+            BitConverter.ToInt32(Microsoft.AspNetCore.WebUtilities.Base64UrlTextEncoder.Decode(code));
 
-            return number;
-        }
+        //public static string EncodeNumber(int number)
+        //{
+        //    var bytes = BitConverter.GetBytes(number);
+        //    var base64String = Convert.ToBase64String(bytes);
+        //    var encodedString = System.Web.HttpUtility.UrlEncode(base64String);
+        //    Microsoft.AspNetCore.WebUtilities.Base64UrlTextEncoder.Encode(BitConverter.GetBytes(number));
+        //    return encodedString;
+        //}
+
+        //public static int DecodeNumber(string code)
+        //{
+        //    var decodedString = System.Web.HttpUtility.UrlDecode(code);
+        //    var bytes = Convert.FromBase64String(decodedString);
+        //    var number = BitConverter.ToInt32(bytes, 0);
+
+        //    return number;
+        //}
     }
 }
