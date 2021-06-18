55a60b7ac80f53c62fd3f6a7bf7ac55aed4fc7b6
mohsen <mohsen1299@gmail.com>
2021-06-18 11:09:17 +0430

    add ClientCategory class

---
+++ Peikresan/Data/ClientModels/ClientCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace Peikresan.Data.ClientModels
+{
+    public class ClientCategory
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int ParentId { get; set; }
+    }
+}
