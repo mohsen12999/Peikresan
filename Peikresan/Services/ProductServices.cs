6892898ded3c83eeb197aa081bcfaccbd6ef798f
mohsen <mohsen1299@gmail.com>
2021-06-18 11:07:33 +0430

    add ProductServices and GetAllProducts method

---
+++ Peikresan/Services/ProductServices.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Peikresan.Data;
+using Peikresan.Data.ClientModels;
+
+namespace Peikresan.Services
+{
+    public static class ProductServices
+    {
+        public static async Task<List<ClientProduct>> GetAllProducts(ApplicationDbContext context)
+            => await context.Products
+                .Include(p=>p.Category)
+                .Select(product => product.ConvertToClientProduct())
+                .AsNoTracking()
+                .ToListAsync();
+    }
+}
