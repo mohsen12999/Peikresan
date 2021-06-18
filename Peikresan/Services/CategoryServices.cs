7d0703fd3de7b391e3213066bb42c61d50f4f22f
mohsen <mohsen1299@gmail.com>
2021-06-18 11:14:03 +0430

    add CategoryServices and GetAllCategories in it

---
+++ Peikresan/Services/CategoryServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Peikresan.Data;
+using Peikresan.Data.ClientModels;
+
+namespace Peikresan.Services
+{
+    public static class CategoryServices
+    {
+        public static async Task<List<ClientCategory>> GetAllCategories(ApplicationDbContext context)
+            => await context.Categories
+                .Select(category => category.ConvertToClientCategory())
+                .AsNoTracking()
+                .ToListAsync();
+    }
+}
