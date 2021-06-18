d45ca432850ee9a9f979d7aff8e5db1b27f79927
mohsen <mohsen1299@gmail.com>
2021-06-18 11:09:35 +0430

    add ClientSellerProduct class

---
+++ Peikresan/Data/ClientModels/ClientSellerProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peikresan.Data.ClientModels
+{
+    public class ClientSellerProduct
+    {
+        public int ProductId { get; set; }
+        public string ProductTitle { get; set; }
+
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+    }
+}
