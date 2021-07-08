64ca61a9e515304d9322374a6c84846086bd8257
mohsen <mohsen1299@gmail.com>
2021-07-08 23:53:31 +0430

    change comment model

--- Peikresan/Data/Models/Comment.cs
+++ Peikresan/Data/Models/Comment.cs
@@ -8,7 +8,11 @@ namespace Peikresan.Data.Models
     public class Comment
     {
         public int Id { get; set; }
-        public int Text { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public int Score { get; set; }
 
         public bool Accept { get; set; } = false;
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
