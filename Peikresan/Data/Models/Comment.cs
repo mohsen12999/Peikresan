81df22b5d2119edda69632536523c2ba4e8fb89b
mohsen <mohsen1299@gmail.com>
2021-07-08 21:29:50 +0430

    add comments model

---
+++ Peikresan/Data/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peikresan.Data.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int Text { get; set; }
+
+        public bool Accept { get; set; } = false;
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
