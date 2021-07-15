fef549ee036c094484909392d982699c82cdb23e
mohsen <mohsen1299@gmail.com>
2021-07-15 18:12:30 +0430

    make comment service for comment model functions

---
+++ Peikresan/Services/CommentServices.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Peikresan.Data;
+using Peikresan.Data.Dto;
+
+namespace Peikresan.Services
+{
+    public class CommentServices
+    {
+        public static async Task<List<CommentDto>> GetAllComments(ApplicationDbContext context)
+            => await context.Comments
+                .Include(c => c.Product)
+                .Select(comment => comment.ToDto())
+                .AsNoTracking()
+                .ToListAsync();
+    }
+}
