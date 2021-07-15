dfb2f9fc02f3d6cf9e2d08ebc39a36e81b0d2533
mohsen <mohsen1299@gmail.com>
2021-07-15 18:12:02 +0430

    make comment dto model

---
+++ Peikresan/Data/Dto/CommentDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peikresan.Data.Dto
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public int Score { get; set; }
+
+        public bool Accept { get; set; } = false;
+        public string CreateDateTime { get; set; }
+
+        public int ProductId { get; set; }
+        public string Product { get; set; }
+    }
+}
